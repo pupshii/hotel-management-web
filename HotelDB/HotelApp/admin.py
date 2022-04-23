627e70b1f3ae1f7c3380344356b4e03beec4779f
Panichito <panithi2001big@gmail.com>
2022-04-24 00:03:42 +0700

    register all models

--- HotelDB/HotelApp/admin.py
+++ HotelDB/HotelApp/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
 from .models import *
+from django.apps import apps
+
+app=apps.get_app_config('HotelApp')
+for model_name, model in app.models.items():
+    admin.site.register(model)
 
 admin.site.site_header='CPE231 Database Systems - Hotel Management Admin Site'
 # admin.autodiscover()
 # admin.site.enable_nav_sidebar=False
-admin.site.register(AllBook)
-admin.site.register(GetNews)
-admin.site.register(Hotel)
-admin.site.register(Member)
-admin.site.register(News)
-admin.site.register(Payment)
-admin.site.register(Promotion)
-admin.site.register(Room)
-admin.site.register(RoomType)
-admin.site.register(StaffManager)
-admin.site.register(Transaction)
